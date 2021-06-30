e45ccbbf1002f8cf37aa8ac6b236d465b0c966d0
Coding in Flow <info@codinginflow.com>
2021-06-30 09:53:39 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] float movementSpeed = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+        LookAtMouse();
+    }
+
+    void LookAtMouse()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+        if (plane.Raycast(mouseRay, out float hitDist))
+        {
+            Vector3 hitPoint = mouseRay.GetPoint(hitDist);
+            transform.LookAt(hitPoint);
+        }
+    }
+}
